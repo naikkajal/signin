242bbea638e108c0a1b5636bf1f138db336167af
Kajal Naik <naikkajal0603@gmail.com>
2024-07-03 23:34:37 +0530

    loginpage complete

--- App.js
+++ App.js
@@ -1,36 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/login';
-import Signin from './src/screens/signin';
+import Signup from './src/screens/signup';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <><NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ title: 'Welcome' }} />
-      <Stack.Screen
-          name="Signin"
-          component={Signin}
-          options={{ title: 'Welcome' }} />
-      </Stack.Navigator>
-    </NavigationContainer><View style={styles.container}>
-        <Text></Text>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ title: 'Welcome' }} />
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{ title: 'Welcome' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <View style={styles.container}>
         <StatusBar style="auto" />
-      </View></>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
