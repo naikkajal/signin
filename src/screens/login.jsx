ba6d82a41be32bbe956b41263e1a23b93bf5981e
Kajal Naik <naikkajal0603@gmail.com>
2024-07-03 11:04:28 +0530

    icons

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image, TextInput  } from 'react-native'
 import React from 'react'
+import Entypo from '@expo/vector-icons/Entypo';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View>
+        <Image source={require("../assets/topimg.png")} style={styles.image} />
+        <View style={styles.hellocontainer}>
+          <Text style={styles.hellotext}>Welcome</Text>
+        </View>
+        <View>
+          <Text style={styles.signintext}>Sign in to your account</Text>
+        </View>
+        <View style={styles.usernamecontainer}>
+        <Entypo name="user" size={24} color="black" style={styles.usericon} />
+        <TextInput style={styles.usernametext}/>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:"white",
+    flex:1
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  hellocontainer:{
+   
+  },
+  hellotext:{
+    textAlign:"center",
+    fontSize:50,
+    fontWeight:"bold"
+  },
+  signintext:{
+    textAlign:"center",
+    fontSize:18,
+    fontWeight:"500",
+  },
+  usernamecontainer:{},
+  usernametext:{}
+})
